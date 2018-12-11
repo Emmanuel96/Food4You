a7d5948a7bed633404dc14a958d7068bb06717b4
Emmanuel Audu <eaudu@gidimobile.com>
2018-12-12 00:36:07 +0100

    changes to checkout controller and added the bes club sandwich and french toast club sandwich

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -24,9 +24,12 @@ class CheckoutController extends Controller
 {
     public function displayCheckout(){
         Log::info('I got here');
-        if(!Session::has('cart'))
+        // return Session::get('cart');
+        if(Session::get('cart') == "")
         {
-            return view('cartViews.cart');
+            // return view('cartViews.cart');
+            //if no cart, return back to menu view
+            url()->previous();
         }
         $oldCart = Session::get('cart');
         $cart = new Cart($oldCart);
