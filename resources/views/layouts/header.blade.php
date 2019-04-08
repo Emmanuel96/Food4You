3771d298dc38a34d6d9a0f04144cc3a30a504b6d
Emmanuel96 <emmanuel.audu1@aun.edu.ng>
2019-04-08 06:39:43 +0100

    fixed issue with header blade file & order tracking is now responsive

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -63,9 +63,7 @@
 
         <style>
 
-            button .navbar-toggle{
-                margin-right: 30px;
-            }
+           
 
             .flat-table {
                 display: block;
@@ -302,7 +300,7 @@
     <nav style="background-color:00000;" id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll" style="margin-right:30px;">
+            <div class="navbar-header page-scroll" >
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" >
                     <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
                 </button>
