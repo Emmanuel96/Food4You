27e89349f6499626577612447b702c5522875517
Emmanuel Audu <eaudu@gidimobile.com>
2019-08-01 20:04:56 +0100

    changed header from naijabites back to wailodile

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -311,7 +311,7 @@
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" >
                     <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand page-scroll" href="/home"><i class = "fa fa-cutlery"></i>Naija Bites</a>
+                <a class="navbar-brand page-scroll" href="/home"><i class = "fa fa-cutlery"></i>Wailodile</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
